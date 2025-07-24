f7f75c64833f26b63d66b35fc3407d15b7242037
novinthen <42584998+novinthen@users.noreply.github.com>
2025-07-24 13:41:40 +0800

    Create constants.js
    create constants.js

---
+++ src/constants.js
@@ -0,0 +1,25 @@
+export const APP_ID = 'keadilan-wp-info';
+
+export const CABANGS = [
+  'KEPONG',
+  'SEGAMBUT',
+  'BATU',
+  'WANGSA MAJU',
+  'SETIAWANGSA',
+  'TITIWNAGSA',
+  'BUKIT BINTANG',
+  'LEMBANG PANTAI',
+  'SEPUTEH',
+  'CHERAS',
+  'BANDAR TUN RAZAK',
+];
+
+export const ROLES = [
+  'KC',
+  'WK',
+];
+
+export const PATHS = {
+  USERS: 'users',
+  NEWS: 'news',
+};
