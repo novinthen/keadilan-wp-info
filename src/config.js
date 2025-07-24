f8c51a0ddfdef9c8e977b12f40b8d272b7970464
novinthen <42584998+novinthen@users.noreply.github.com>
2025-07-24 14:00:05 +0800

    Create config.js
    grok 5

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// src/config.js
+window.__firebase_config = JSON.stringify({
+    apiKey: "AIzaSyBdJcoiHKZf4g8bfNs_hwQ2te5BCNVwQkM",
+    authDomain: "keadilan-wp-info.firebaseapp.com",
+    projectId: "keadilan-wp-info",
+    storageBucket: "keadilan-wp-info.firebasestorage.app",
+    messagingSenderId: "1097147310925",
+    appId: "1:1097147310925:web:d7ef566764b1b859220d04"
+  
+});
